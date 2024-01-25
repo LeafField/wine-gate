9b45b3e6e1b4434072e1a41f2f74a5b1a473f6a5
LeafField <109466326+LeafField@users.noreply.github.com>
2024-01-25 10:11:30 +0900

    SeachSortコンポーネントの実装

--- src/components/test-components/DummyForm.tsx
+++ src/components/test-components/DummyForm.tsx
@@ -1,14 +1,27 @@
-import React, { FC, PropsWithChildren, FormEvent } from "react";
+import React, { FC, PropsWithChildren, FormEvent, useState } from "react";
+
+type FormState = {
+  [key: string]: FormDataEntryValue;
+};
 
 const DummyForm: FC<PropsWithChildren> = ({ children }) => {
+  const [input, setInput] = useState<string>("");
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const data = Object.fromEntries(formData.entries());
-    console.log(data);
+    // const data = Object.fromEntries(formData.entries());
+    const data = formData.get("search");
+    if (data) {
+      setInput(data.toString());
+    }
   };
 
-  return <form onSubmit={submitHandler}>{children}</form>;
+  return (
+    <form onSubmit={submitHandler}>
+      {children}
+      <p>{input}</p>
+    </form>
+  );
 };
 
 export default DummyForm;
