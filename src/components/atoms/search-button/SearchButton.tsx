b0f57cfdd14958ba1108bfd7c72d468d9dfe27c3
LeafField <109466326+LeafField@users.noreply.github.com>
2024-01-27 05:16:54 +0900

    SearchButtonコンポーネントの作成

---
+++ src/components/atoms/search-button/SearchButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchButton = () => {
+  return (
+    <button
+      type="submit"
+      className="border-2 border-gray px-3 py-1 transition-colors duration-300 hover:border-blue focus:border-blue focus:outline-none"
+    >
+      検索
+    </button>
+  );
+};
+
+export default SearchButton;
