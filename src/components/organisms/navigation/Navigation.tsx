90fbb56d60044fc759c2e5baef66a0ed46e903c4
LeafField <109466326+LeafField@users.noreply.github.com>
2024-02-02 22:11:19 +0900

    ナビゲーションのモバイル時のCSSが崩れていたのを修正しました

--- src/components/organisms/navigation/Navigation.tsx
+++ src/components/organisms/navigation/Navigation.tsx
@@ -13,7 +13,7 @@ const Navigation = () => {
   return (
     <nav className="nav">
       <div
-        className={`${menu === "close" ? "invisible" : "show-panel"} top-1 z-nav tablet:visible tablet:sticky tablet:space-y-2`}
+        className={`${menu === "close" ? "invisible" : "show-panel"} z-nav tablet:visible tablet:sticky tablet:top-1 tablet:space-y-2`}
         id="navigation"
       >
         <UserPanel />
