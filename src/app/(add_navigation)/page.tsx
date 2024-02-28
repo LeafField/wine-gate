6f373180d708efe74866378adf4bcad9402b2e1d
LeafField <109466326+LeafField@users.noreply.github.com>
2024-02-28 19:57:25 +0900

    revalidateの数字の変更

--- src/app/(add_navigation)/page.tsx
+++ src/app/(add_navigation)/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import TopPage from "../../components/template/top-page/TopPage";
 import { getNewWines } from "../../utils/fetcher";
 
-export const revalidate = 60 * 60;
+export const revalidate = 3600;
 export const runtime = "edge";
 
 const Home = async () => {
