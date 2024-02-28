46dcf7b126fc8e791f95e6c06270ca424d35cf5c
LeafField <109466326+LeafField@users.noreply.github.com>
2024-02-28 20:43:47 +0900

    固定ページのruntimeを実験的に削除

--- src/app/(add_navigation)/page.tsx
+++ src/app/(add_navigation)/page.tsx
@@ -3,7 +3,6 @@ import TopPage from "../../components/template/top-page/TopPage";
 import { getNewWines } from "../../utils/fetcher";
 
 export const revalidate = 3600;
-export const runtime = "edge";
 
 const Home = async () => {
   const articles = await getNewWines();
--- src/app/(one_column)/select/page.tsx
+++ src/app/(one_column)/select/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import SelectPage from "../../../components/template/select-page/SelectPage";
 
 export const revalidate = 86400;
-export const runtime = "edge";
 
 const page = async () => {
   return <SelectPage />;
