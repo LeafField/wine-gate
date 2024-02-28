ab6223fb613ef4e31ee3309b5e517739869fbc55
LeafField <109466326+LeafField@users.noreply.github.com>
2024-02-28 21:32:45 +0900

    isgの動作確認

--- src/app/(add_navigation)/article/[id]/page.tsx
+++ src/app/(add_navigation)/article/[id]/page.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { getWine } from "../../../../utils/fetcher";
 import ArticlePage from "../../../../components/template/article-page/ArticlePage";
 
-export const runtime = "edge";
+// export const runtime = "edge";
+export const dynamicParams = true;
+
+export const generateStaticParams = async () => {
+  return [{ slug: "4be1f2ab-b3b3-4599-b1a1-a73d280f5033" }];
+};
 
 const page = async ({ params }: { params: { id: string } }) => {
   const wine = await getWine(params.id);
