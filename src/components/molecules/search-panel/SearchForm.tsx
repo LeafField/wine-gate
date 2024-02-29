77180cd1b6358a1f33cc186433064f85d7ddb128
LeafField <109466326+LeafField@users.noreply.github.com>
2024-02-29 22:47:44 +0900

    searchInputの不具合の修正

--- src/components/molecules/search-panel/SearchForm.tsx
+++ src/components/molecules/search-panel/SearchForm.tsx
@@ -3,8 +3,6 @@ import React, { FC, FormEvent, useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import SearchButton from "../../atoms/search-button/SearchButton";
 import SearchInput from "../../atoms/search-input/SearchInput";
-import SearchSort from "../../atoms/search-sort/SearchSort";
-import SearchCategory from "../../atoms/search-category/SearchCategory";
 import { ComboboxData } from "@mantine/core";
 import { searchSchema } from "../../../utils/schema";
 import { useStore } from "../../../store";
@@ -18,7 +16,7 @@ const SearchPanel: FC<Props> = ({ selectData }) => {
   const [errorState, setErrorState] = useState<string | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
-  const { menu } = useStore();
+  const { menu, clearSearchValue } = useStore();
   const queryClient = useQueryClient();
 
   const submitHandler = (event: FormEvent<HTMLFormElement>) => {
@@ -36,6 +34,7 @@ const SearchPanel: FC<Props> = ({ selectData }) => {
     } else {
       setErrorState(schema.error.errors[0].message);
     }
+    clearSearchValue();
   };
 
   return (
