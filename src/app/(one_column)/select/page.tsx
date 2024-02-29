2191647c00b02e4269e7c4b0ce0dab3037ec741d
LeafField <109466326+LeafField@users.noreply.github.com>
2024-02-29 14:11:34 +0900

    selectpageのrevalidateの設定変更

--- src/app/(one_column)/select/page.tsx
+++ src/app/(one_column)/select/page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import SelectPage from "../../../components/template/select-page/SelectPage";
+import { categoryFavoriteSmallArticleFetcher } from "../../../utils/categoryFetcher";
 
-export const revalidate = 86400;
+export const revalidate = 3600;
 
 const page = async () => {
-  return <SelectPage />;
+  const sober = await categoryFavoriteSmallArticleFetcher("sober");
+  const fruity = await categoryFavoriteSmallArticleFetcher("fruity");
+  const tart = await categoryFavoriteSmallArticleFetcher("tart");
+  const white = await categoryFavoriteSmallArticleFetcher("white");
+  const sweet = await categoryFavoriteSmallArticleFetcher("sweet");
+  return (
+    <SelectPage
+      fruity={fruity}
+      sober={sober}
+      sweet={sweet}
+      tart={tart}
+      white={white}
+    />
+  );
 };
 
 export default page;
