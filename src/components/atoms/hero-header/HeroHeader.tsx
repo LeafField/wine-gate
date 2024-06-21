b411eec7c1e85d50ef081550cc98a12e99915dfc
LeafField <109466326+LeafField@users.noreply.github.com>
2024-06-21 14:25:05 +0900

    カテゴリーリンクのプリフェッチをfalseにしました

--- src/components/atoms/hero-header/HeroHeader.tsx
+++ src/components/atoms/hero-header/HeroHeader.tsx
@@ -7,7 +7,7 @@ type Props = {
 const HeroHeader: FC<Props> = ({ title }) => {
   return (
     // <div className="absolute inset-0 z-front flex h-full w-full items-center justify-end bg-gradient-to-r from-transparent from-20% to-black/75 to-90% pr-6 ">
-    <div className="hero-mobile-verlay tablet:hero-overlay absolute inset-0 z-front flex h-full w-full items-center justify-end pr-6">
+    <div className="absolute inset-0 z-front flex h-full w-full items-center justify-end pr-6 hero-mobile-verlay tablet:hero-overlay">
       <h2 className="text-heading2 font-bold text-white">{title}</h2>
     </div>
   );
