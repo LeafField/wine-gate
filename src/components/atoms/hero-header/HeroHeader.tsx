48b496582c34087e4efd9191ffbdf9c42489d915
LeafField <109466326+LeafField@users.noreply.github.com>
2024-08-24 22:36:16 +0900

    高解像度モニターでのhero-headerの表示の不具合の修正

--- src/components/atoms/hero-header/HeroHeader.tsx
+++ src/components/atoms/hero-header/HeroHeader.tsx
@@ -6,9 +6,10 @@ type Props = {
 
 const HeroHeader: FC<Props> = ({ title }) => {
   return (
-    // <div className="absolute inset-0 z-front flex h-full w-full items-center justify-end bg-gradient-to-r from-transparent from-20% to-black/75 to-90% pr-6 ">
-    <div className="absolute inset-0 z-front flex h-full w-full items-center justify-end pr-6 hero-mobile-verlay tablet:hero-overlay">
-      <h2 className="text-heading2 font-bold text-white">{title}</h2>
+    <div className="hero-mobile-overlay absolute inset-0 z-front flex h-full w-full items-center justify-end tablet:hero-overlay">
+      <div className="mx-auto flex w-full max-w-[87rem] items-center justify-end px-6">
+        <h2 className="text-heading2 font-bold text-white">{title}</h2>
+      </div>
     </div>
   );
 };
