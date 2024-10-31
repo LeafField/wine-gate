16aa333c400b218c1a3d26b7814250944fdd66f8
LeafField <109466326+LeafField@users.noreply.github.com>
2024-10-31 19:51:07 +0900

    prefetch false testing

--- src/components/atoms/login-button/LoginButton.tsx
+++ src/components/atoms/login-button/LoginButton.tsx
@@ -6,6 +6,7 @@ const LoginButton = () => {
     <Link
       className="w-full rounded-xl border border-gray py-2 text-center transition-[border-color] duration-300 hover:border-blue focus:outline-blue"
       href={"/login"}
+      prefetch={false}
     >
       ログイン/会員登録
     </Link>
